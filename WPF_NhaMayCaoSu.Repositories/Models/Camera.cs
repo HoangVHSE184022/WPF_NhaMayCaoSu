5e01071a9f119516b612efbb4bac46104e599509
NhatMinhz <trinhhhoangnhatminh123@gmail.com>
2024-08-28 18:46:37 +0700

    ChangeStatusInsteadOfDelete2

--- WPF_NhaMayCaoSu.Repositories/Models/Camera.cs
+++ WPF_NhaMayCaoSu.Repositories/Models/Camera.cs
@@ -5,5 +5,6 @@
         public Guid CameraId { get; set; }
         public string Camera1 { get; set; }
         public string Camera2 { get; set; }
+        public short Status { get; set; }
     }
 }
