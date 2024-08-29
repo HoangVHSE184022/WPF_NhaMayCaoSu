3b3a69c8467689956bb794d408221d216bf3e0a5
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-08-29 09:51:27 +0700

    add broker management

---
+++ WPF_NhaMayCaoSu/BrokerWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_NhaMayCaoSu
+{
+    /// <summary>
+    /// Interaction logic for BrokerWindow.xaml
+    /// </summary>
+    public partial class BrokerWindow : Window
+    {
+        public BrokerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
