6cc8d79a969cc2af818295c8f26819c8f026ed35
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-08-29 22:38:20 +0700

    method for customer

--- WPF_NhaMayCaoSu/CustomerManagementWindow.xaml.cs
+++ WPF_NhaMayCaoSu/CustomerManagementWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using WPF_NhaMayCaoSu.Repository.Models;
+using WPF_NhaMayCaoSu.Service.Services;
 
 namespace WPF_NhaMayCaoSu
 {
@@ -7,6 +9,10 @@ namespace WPF_NhaMayCaoSu
     /// </summary>
     public partial class CustomerManagementWindow : Window
     {
+        private CustomerService _service = new();
+
+        public Customer SelectedCustomer { get; set; } = null;
+
         public CustomerManagementWindow()
         {
             InitializeComponent();
@@ -17,9 +23,40 @@ namespace WPF_NhaMayCaoSu
             this.Close();
         }
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Chưa làm đâu bố", "Chưa làm!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            Customer x = new();
+            x.CustomerName = AccountNameTextBox.Text;
+            x.RFIDCode = long.Parse(RFIDCodeTextBox.Text);
+            x.Status = short.Parse(StatusTextBox.Text);
+
+            if (SelectedCustomer == null)
+            {
+                x.CreatedDate = DateTime.Now;
+                x.ExpirationDate = DateTime.Now.AddDays(1);
+
+                MessageBox.Show($"Created {AccountNameTextBox.Text}!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                await _service.CreateCustomer(x);
+            }
+            else
+            {
+                MessageBox.Show($"Updated {SelectedCustomer.CustomerName}!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                await _service.UpdateCustomer(x);
+            }
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ModeLabel.Content = "Thêm khách hàng mới";
+
+            if (SelectedCustomer != null)
+            {
+                AccountNameTextBox.Text = SelectedCustomer.CustomerName;
+                RFIDCodeTextBox.Text = SelectedCustomer.RFIDCode.ToString();
+                StatusTextBox.Text = SelectedCustomer.Status.ToString();
+                ModeLabel.Content = "Chỉnh sửa khách hàng";
+            }
         }
     }
 }
