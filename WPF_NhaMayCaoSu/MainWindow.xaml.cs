67e594b5260dec468f96931ac6c6ac6ec02f26d0
Võ Huy Hoàng <145216389+HoangVHSE184022@users.noreply.github.com>
2024-08-30 21:39:57 +0700

    push demo

--- WPF_NhaMayCaoSu/MainWindow.xaml.cs
+++ WPF_NhaMayCaoSu/MainWindow.xaml.cs
@@ -69,6 +69,9 @@ namespace WPF_NhaMayCaoSu
             dualCameraWindow.ShowDialog();
         }
 
-       
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
