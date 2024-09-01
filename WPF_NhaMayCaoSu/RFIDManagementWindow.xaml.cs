01d4b27cc6bbeee37ee70d10ca8eafa8915e50ad
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2024-09-01 10:46:14 +0700

    fill data

--- WPF_NhaMayCaoSu/RFIDManagementWindow.xaml.cs
+++ WPF_NhaMayCaoSu/RFIDManagementWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,7 @@ namespace WPF_NhaMayCaoSu
         private RFIDService _service = new();
         private CustomerService _customerService = new();
         public RFID SelectedRFID { get; set; } = null;
+        private MqttClientService _mqttClientService = new MqttClientService();
         public RFIDManagementWindow()
         {
             InitializeComponent();
@@ -33,6 +35,10 @@ namespace WPF_NhaMayCaoSu
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             ModeLabel.Content = "Thêm RFID mới";
+            await _mqttClientService.ConnectAsync();
+            await _mqttClientService.SubscribeAsync("CreateRFID");
+
+            _mqttClientService.MessageReceived += OnMqttMessageReceived;
 
             CustomerComboBox.ItemsSource = await _customerService.GetAllCustomers(1,100);
 
@@ -45,6 +51,37 @@ namespace WPF_NhaMayCaoSu
                 ExpDateDatePicker.Text = SelectedRFID.ExpirationDate.ToString();
                 StatusTextBox.Text = SelectedRFID.Status.ToString();
                 ModeLabel.Content = "Chỉnh sửa RFID";
+            }
+        }
+
+        private void OnMqttMessageReceived(object sender, string data)
+        {
+            try
+            {
+                if (data.StartsWith("CreateRFID:"))
+                {
+                    string rfidString = data.Substring("CreateRFID:".Length);
+
+                    if (!string.IsNullOrEmpty(rfidString))
+                    {
+                        RFIDCodeTextBox.Dispatcher.Invoke(() =>
+                        {
+                            RFIDCodeTextBox.Text = rfidString;
+                        });
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Failed to parse RFID number.");
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Unexpected message format.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error processing message: {ex.Message}");
             }
         }
 
