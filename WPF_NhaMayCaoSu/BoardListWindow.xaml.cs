3674efe4d8ca1e5fa0f1a5c69a4d9153b7f26d59
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2024-09-09 18:56:21 +0700

    update model

--- WPF_NhaMayCaoSu/BoardListWindow.xaml.cs
+++ WPF_NhaMayCaoSu/BoardListWindow.xaml.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿
 using System.Diagnostics;
 using WPF_NhaMayCaoSu.Core.Utils;
@@ -5,6 +6,25 @@ using System.Windows;
 using WPF_NhaMayCaoSu.Repository.Models;
 using WPF_NhaMayCaoSu.Service.Services;
 
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF_NhaMayCaoSu.Repository.Models;
+using WPF_NhaMayCaoSu.Service.Interfaces;
+using WPF_NhaMayCaoSu.Service.Services;
+>>>>>>> Stashed changes
 
 namespace WPF_NhaMayCaoSu
 {
@@ -13,13 +33,19 @@ namespace WPF_NhaMayCaoSu
     /// </summary>
     public partial class BoardListWindow : Window
     {
+<<<<<<< Updated upstream
         private readonly MqttServerService _mqttServerService;
         private readonly MqttClientService _mqttClientService;
         private List<Board> _sessionBoardList { get; set; } = new();
+=======
+        private readonly MqttClientService _mqttClientService;
+        private readonly MqttServerService _mqttServerService;
+>>>>>>> Stashed changes
         public BoardListWindow()
         {
             InitializeComponent();
             _mqttServerService = MqttServerService.Instance;
+<<<<<<< Updated upstream
             _mqttServerService.ClientsChanged += MqttService_ClientsChanged;
             _mqttClientService = new MqttClientService();
         }
@@ -114,5 +140,59 @@ namespace WPF_NhaMayCaoSu
                 Debug.WriteLine($"Error processing message: {ex.Message}");
             }
         }
+=======
+        }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await _mqttClientService.ConnectAsync();
+                await _mqttClientService.SubscribeAsync("BoardInfo");
+
+
+                _mqttClientService.MessageReceived += (s, data) =>
+                {
+                    OnMqttMessageReceived(s, data);
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể kết nối đến máy chủ MQTT. Vui lòng kiểm tra lại kết nối. Bạn sẽ được chuyển về màn hình quản lý Broker.", "Lỗi kết nối", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                BrokerWindow brokerWindow = new BrokerWindow();
+                brokerWindow.ShowDialog();
+                this.Close();
+                return;
+            }
+        }
+
+        private async void OnMqttMessageReceived(object sender, string data)
+        {
+            try
+            {
+                if (data.StartsWith("BoardInfo:"))
+                {
+                    string messageContent = data.Substring("BoardInfo:".Length);
+                    string[] messages = messageContent.Split(':');
+
+                    if (messages.Length == 2)
+                    {
+                        string MacAddress = messages[0];
+                        int currentMode = int.Parse(messages[1]);
+                        Board connectedBoard = null;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Unexpected message topic.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error processing message: {ex.Message}");
+            }
+        }
+
+>>>>>>> Stashed changes
     }
 }
