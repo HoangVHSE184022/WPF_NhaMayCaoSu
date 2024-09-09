51f4999c08017850406695cfd4039d30be303954
NhatMinhz <trinhhhoangnhatminh123@gmail.com>
2024-09-09 19:12:47 +0700

    CRUDBoard

---
+++ WPF_NhaMayCaoSu.Repositories/Repositories/BoardRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WPF_NhaMayCaoSu.Repository.Context;
+using WPF_NhaMayCaoSu.Repository.IRepositories;
+using WPF_NhaMayCaoSu.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WPF_NhaMayCaoSu.Repository.Repositories
+{
+    public class BoardRepository : IBoardRepository
+    {
+        private CaoSuWpfDbContext _context;
+
+        public async Task CreateBoardAsync(Board board)
+        {
+            _context = new();
+            await _context.Boards.AddAsync(board);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Board>> GetAllBoardsAsync(int pageNumber, int pageSize)
+        {
+            _context = new();
+            return await _context.Boards
+                                 .Skip((pageNumber - 1) * pageSize)
+                                 .Take(pageSize)
+                                 .ToListAsync();
+        }
+
+        public async Task<Board> GetBoardByIdAsync(Guid boardId)
+        {
+            _context = new();
+            return await _context.Boards.FirstOrDefaultAsync(x => x.BoardId == boardId);
+        }
+
+        public async Task UpdateBoardAsync(Board board)
+        {
+            _context = new();
+            _context.Boards.Update(board);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBoardAsync(Guid boardId)
+        {
+            _context = new();
+            Board board = await _context.Boards.FirstOrDefaultAsync(x => x.BoardId == boardId);
+            if (board != null)
+            {
+                _context.Boards.Remove(board);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Board> GetBoardByNameAsync(string boardName)
+        {
+            _context = new();
+            return await _context.Boards.FirstOrDefaultAsync(x => x.BoardName == boardName);
+        }
+
+        public async Task<int> GetTotalBoardsCountAsync()
+        {
+            _context = new();
+            return await _context.Boards.CountAsync();
+        }
+    }
+}
---
+++ WPF_NhaMayCaoSu.Service/Services/BoardService.cs
@@ -0,0 +1,50 @@
+﻿using WPF_NhaMayCaoSu.Repository.IRepositories;
+using WPF_NhaMayCaoSu.Repository.Models;
+using WPF_NhaMayCaoSu.Repository.Repositories;
+using WPF_NhaMayCaoSu.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WPF_NhaMayCaoSu.Service.Services
+{
+    public class BoardService : IBoardService
+    {
+        private readonly IBoardRepository _boardRepository = new BoardRepository();
+
+        public async Task CreateBoardAsync(Board board)
+        {
+            await _boardRepository.CreateBoardAsync(board);
+        }
+
+        public async Task<IEnumerable<Board>> GetAllBoardsAsync(int pageNumber, int pageSize)
+        {
+            return await _boardRepository.GetAllBoardsAsync(pageNumber, pageSize);
+        }
+
+        public async Task<Board> GetBoardByIdAsync(Guid boardId)
+        {
+            return await _boardRepository.GetBoardByIdAsync(boardId);
+        }
+
+        public async Task UpdateBoardAsync(Board board)
+        {
+            await _boardRepository.UpdateBoardAsync(board);
+        }
+
+        public async Task DeleteBoardAsync(Guid boardId)
+        {
+            await _boardRepository.DeleteBoardAsync(boardId);
+        }
+
+        public async Task<Board> GetBoardByNameAsync(string boardName)
+        {
+            return await _boardRepository.GetBoardByNameAsync(boardName);
+        }
+
+        public async Task<int> GetTotalBoardsCountAsync()
+        {
+            return await _boardRepository.GetTotalBoardsCountAsync();
+        }
+    }
+}
