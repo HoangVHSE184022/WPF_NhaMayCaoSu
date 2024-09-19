1cc0cef94d065d862be92bcb54a2bcdac765dcb0
NhatMinhz <trinhhhoangnhatminh123@gmail.com>
2024-09-19 12:34:25 +0700

    SelectNameForBoard

---
+++ WPF_NhaMayCaoSu/SaveBoardWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_NhaMayCaoSu
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class SaveBoardWindow : Window
+    {
+        public string SelectedBoardName { get; private set; }
+
+        public SaveBoardWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (BoardNameComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                SelectedBoardName = selectedItem.Content.ToString();
+                DialogResult = true; 
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một tên cho board.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
