18894e4f58bbb1a7b7b4322b890f1ce9a90beef2
Hyudeque <quanghuy01062004@gmail.com>
2024-10-25 16:38:15 +0700

    RFID default

--- WPF_NhaMayCaoSu.Repositories/Models/RFID.cs
+++ WPF_NhaMayCaoSu.Repositories/Models/RFID.cs
@@ -5,7 +5,7 @@ namespace WPF_NhaMayCaoSu.Repository.Models
     public class RFID
     {
         public Guid RFID_Id { get; set; }
-        public string RFIDCode { get; set; }
+        public string RFIDCode { get; set; } = "000000000";
         public DateTime CreatedDate { get; set; }
         public DateTime ExpirationDate { get; set; }
         public Guid CustomerId { get; set; }
